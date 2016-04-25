5ec0c4e48cc37d96ffd1ef118d0955d3d63edc45
fan CDA <fanCDApozdrawia@gmail.com>
2016-04-25 01:01:28 +0100

    Add DataSource class where we gonna store a data and create dummy TwoPages class. Add simple ListBox in MainWindow and bind dummy data to see if it works.

---
+++ ProjectCDA/Data/DataSource.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCDA.Data
+{
+    public class DataSource : INotifyPropertyChanged
+    {
+        private ObservableCollection<TwoPages> _Schedule;
+
+        public DataSource()
+        {
+            AddSomeDummyData();
+        }
+
+        public ObservableCollection<TwoPages> Schedule
+        {
+            get
+            {
+                return _Schedule;
+            }
+            set
+            {
+                if (value != _Schedule)
+                {
+                    _Schedule = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Schedule"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+
+
+        private void AddSomeDummyData()
+        {
+            ObservableCollection<TwoPages> TmpSchedule = new ObservableCollection<TwoPages>();
+
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+            TmpSchedule.Add(new TwoPages());
+
+            Schedule = TmpSchedule;
+        }
+    }
+}
