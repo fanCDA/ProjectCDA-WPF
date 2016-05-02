8804b9a7fa03cf9062a5b0fac2b34c5e94173723
fan CDA <fanCDApozdrawia@gmail.com>
2016-05-02 20:30:16 +0100

    Moving our ItemsControl itemTemplate to separate file.

---
+++ ProjectCDA/UserControls/TwoPagesTemplateControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProjectCDA.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TwoPagesTemplateControl.xaml
+    /// </summary>
+    public partial class TwoPagesTemplateControl : UserControl
+    {
+        public TwoPagesTemplateControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
