6b727e48c20828861eb14daa45a204a1e168064c
fan CDA <fanCDApozdrawia@gmail.com>
2016-05-03 03:27:17 +0100

    Add some fancy converter for page number value based on ID.

---
+++ ProjectCDA/Data/Converters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjectCDA.Data
+{
+    public class IDToLeftPageNumber : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 2 * (int)value + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IDToRightPageNumber : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 2 * (int)value + 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
