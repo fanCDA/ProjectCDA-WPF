8f95d3f8c90100dc44c8e35cfe9acd95add88ae2
fan CDA <fanCDApozdrawia@gmail.com>
2016-05-05 02:44:58 +0100

    Replace 2 Borders with TextBoxes inside TwoPagesTemplateDefault with 2 times SinglePageControl. Use ContentControl to display data of single object (template coming soon).

---
+++ ProjectCDA/UserControls/SinglePageControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProjectCDA.UserControls
+{
+    /// <summary>
+    /// Interaction logic for SinglePageControl.xaml
+    /// </summary>
+    public partial class SinglePageControl : UserControl
+    {
+        public SinglePageControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
