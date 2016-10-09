cb4f0d6452b4529c9a631501f2f9d64685543d1c
fan CDA <fanCDApozdrawia@gmail.com>
2016-10-09 03:18:09 +0100

    Adding ContentTemplateSelector in SinglePageControl. Adding SinglePageTemplateSelector which is returning SinglePageTemplateDefault (empty single page with input field).

---
+++ ProjectCDA/UserControls/SinglePageTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using ProjectCDA.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProjectCDA.UserControls
+{
+    class SinglePageTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            SinglePage singlePage = item as SinglePage;
+            FrameworkElement element = container as FrameworkElement;
+
+            if (singlePage == null || element == null)
+                return null;
+
+            return element.FindResource("SinglePageTemplateDefault") as DataTemplate;
+        }
+    }
+}
