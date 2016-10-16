f65c008bc161644b9094ac76ab92f0bc5e03a108
fan CDA <fanCDApozdrawia@gmail.com>
2016-10-17 00:53:39 +0100

    Splitting Data/Converters class and moving it to Converters folder. Fixing usage.

---
+++ ProjectCDA/Converter/LeftPageIDConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjectCDA.Converter
+{
+    public class LeftPageIDConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 2 * (int)value + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectCDA/Converter/RightPageIDConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProjectCDA.Converter
+{
+    public class RightPageIDConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 2 * (int)value + 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
