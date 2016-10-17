839e0cc53a5d531dc5db961418249986150062c7
fan CDA <fanCDApozdrawia@gmail.com>
2016-10-17 02:17:11 +0100

    Moving SinglePage Template to View folder.

---
+++ ProjectCDA/View/Templates/SinglePage/SinglePageTemplateDefault.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProjectCDA.View.Templates.SinglePage
+{
+    /// <summary>
+    /// Interaction logic for SinglePageTemplateDefault.xaml
+    /// </summary>
+    public partial class SinglePageTemplateDefault : UserControl
+    {
+        public SinglePageTemplateDefault()
+        {
+            InitializeComponent();
+        }
+    }
+}
