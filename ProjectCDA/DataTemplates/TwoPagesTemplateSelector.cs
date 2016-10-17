4c2b86852a1b5b801b677306f4f535b26d3bea4d
fan CDA <fanCDApozdrawia@gmail.com>
2016-10-17 01:20:46 +0100

    Moving SinglePage and TwoPages template selectors to DataTemplates folder.

--- ProjectCDA/DataTemplates/TwoPagesTemplateSelector.cs
+++ ProjectCDA/DataTemplates/TwoPagesTemplateSelector.cs
@@ -2,7 +2,7 @@
 using System.Windows;
 using System.Windows.Controls;
 
-namespace ProjectCDA.UserControls
+namespace ProjectCDA.DataTemplates
 {
     class TwoPagesTemplateSelector : DataTemplateSelector
     {
