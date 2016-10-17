14507b96583158b05ca39b4acec6c9dde60d68b8
fan CDA <fanCDApozdrawia@gmail.com>
2016-10-17 02:41:05 +0100

    Making MainWindow a bit nicer by moving FacingPages Template selector and DataTemplates out of it.

---
+++ ProjectCDA/View/Page/FacingPagesControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProjectCDA.View.Page
+{
+    /// <summary>
+    /// Interaction logic for FacingPagesControl.xaml
+    /// </summary>
+    public partial class FacingPagesControl : UserControl
+    {
+        public FacingPagesControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
