0fc27f50ec5e778e731fbb5f88af1faf6c239f56
fan CDA <fanCDApozdrawia@gmail.com>
2016-10-23 21:26:40 +0100

    Removing DataSource class from Main project and trying to replace it with MainViewModel (MainViewModel is getting data from DAL). Adding MainViewModel to Application.Resources. Binding Window DataContext to StaticResource MainViewModel instead of DataSource.

---
+++ ProjectCDA/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using ProjectCDA.DAL;
+using ProjectCDA.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ProjectCDA.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly IDataService _dataService;
+
+        public MainViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            Schedule = new ObservableCollection<TwoPages>();
+        }
+
+        public MainViewModel()
+            : this(new DataService())
+        {
+            // This is default constructor
+        }
+
+
+        public void LoadData()
+        {
+            Schedule.Clear();
+
+            var facingPages = _dataService.GetData();
+
+            foreach (var facingPage in facingPages)
+            {
+                Schedule.Add(facingPage);
+            }
+        }
+
+        public ObservableCollection<TwoPages> Schedule
+        {
+            get;
+            private set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
