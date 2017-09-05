ba80507028cdaff920eb22ec74ed920f6b296a2b
fan CDA <fanCDApozdrawia@gmail.com>
2017-09-05 17:51:12 +0100

    Adding extended wpf toolkit so we can use fancy components (like IntegerUpDown). Improve input dialog a bit (add counter with up/down arrows and Default and Minimally button).

--- ProjectCDA/View/Dialogs/InputDialogSample.xaml.cs
+++ ProjectCDA/View/Dialogs/InputDialogSample.xaml.cs
@@ -1,12 +1,24 @@
-﻿using System.Windows;
+using System.Windows;
 
 namespace ProjectCDA.View.Dialogs
 {
     public partial class InputDialogSample : Window
     {
+        private static int DEFAULT_PAGE_NUMBER = 129;
+
         public InputDialogSample()
         {
             InitializeComponent();
+        }
+
+        private void BtnDefault_Click(object sender, RoutedEventArgs e)
+        {
+            intUpDown.Value = DEFAULT_PAGE_NUMBER;
+        }
+
+        private void BtnMin_Click(object sender, RoutedEventArgs e)
+        {
+            intUpDown.Value = intUpDown.Minimum;
         }
     }
 }
