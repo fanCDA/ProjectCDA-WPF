7449b4398203deb4d8f93998f9436627ca58445a
fan CDA <fanCDApozdrawia@gmail.com>
2017-09-08 11:35:34 +0100

    Adding simple drag n drop between single pages with right mouse button.

--- ProjectCDA/View/Templates/SinglePage/SinglePageTemplateDefault.xaml.cs
+++ ProjectCDA/View/Templates/SinglePage/SinglePageTemplateDefault.xaml.cs
@@ -1,15 +1,63 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ProjectCDA.View.Templates.SinglePage
 {
-    /// <summary>
-    /// Interaction logic for SinglePageTemplateDefault.xaml
-    /// </summary>
     public partial class SinglePageTemplateDefault : UserControl
     {
         public SinglePageTemplateDefault()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_PreviewMouseRightButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox != null)
+            {
+                var dataObj = new DataObject(textBox.Text);
+                dataObj.SetData("DragSource", e.Source);
+                DragDrop.DoDragDrop(e.Source as DependencyObject, dataObj, DragDropEffects.Move);
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox_PreviewQueryContinueDrag(object source, System.Windows.QueryContinueDragEventArgs e)
+        {
+            TextBox textBox = source as TextBox;
+
+            e.Handled = true;
+
+            if (e.EscapePressed)
+            {
+                e.Action = DragAction.Cancel;
+                return;
+            }
+
+            if ((e.KeyStates & DragDropKeyStates.LeftMouseButton) != DragDropKeyStates.None)
+            {
+                e.Action = DragAction.Continue;
+                return;
+            }
+
+            if ((e.KeyStates & DragDropKeyStates.RightMouseButton) != DragDropKeyStates.None)
+            {
+                e.Action = DragAction.Continue;
+                return;
+            }
+
+            e.Action = DragAction.Drop;
+            e.Handled = false;
+        }
+
+        private void Tf1_PreviewDrop(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.Text);
+            var dragSource = e.Data.GetData("DragSource");
+
+            (dragSource as TextBox).Text = (sender as TextBox).Text;
+            (sender as TextBox).Text = String.Empty;
         }
     }
 }
